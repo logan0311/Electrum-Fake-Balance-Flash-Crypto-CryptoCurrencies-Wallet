4351b53eb696f1cb3b758b573eeaccbb3ead75fe
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:53:57 +0300

    AccessResult.cs

---
+++ Electrum-Flash/Security/AccessResult.cs
@@ -0,0 +1,23 @@
+﻿using Vulpes.Electrum.Domain.Exceptions;
+
+namespace Vulpes.Electrum.Domain.Security;
+public record AccessResult
+{
+    public static AccessResult Empty { get; } = new();
+
+    public bool AccessGranted { get; init; } = false;
+    public string Message { get; init; } = string.Empty;
+
+    public void ThrowIfAccessDenied()
+    {
+        if (!AccessGranted)
+        {
+            throw new AccessDeniedException(this);
+        }
+    }
+
+    public static implicit operator bool(AccessResult accessResult) => accessResult.AccessGranted;
+
+    public static AccessResult Success() => new() { AccessGranted = true };
+    public static AccessResult Fail(string message) => new() { AccessGranted = false, Message = message };
+}
