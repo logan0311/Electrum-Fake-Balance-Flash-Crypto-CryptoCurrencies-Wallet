5edb7cb8ad8c5f343c1ecd63fecfdf0dbe758004
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:13 +0300

    ConsoleCommandHandler.cs

---
+++ Electrum-Flash/ConsoleCommanding/ConsoleCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Vulpes.Electrum.Domain.Security;
+
+namespace Vulpes.Electrum.Domain.ConsoleCommanding;
+public abstract class ConsoleCommandHandler
+{
+    public abstract string CommandDocumentation { get; }
+    public abstract string CommandName { get; }
+
+    public abstract string SuccessMessage(ConsoleCommand consoleCommand);
+    public abstract Task ExecuteAsync(ConsoleCommand consoleCommand);
+
+    public virtual ElectrumValidationResult ValidateCommand(ConsoleCommand consoleCommand) => ElectrumValidationResult.Verify();
+}
