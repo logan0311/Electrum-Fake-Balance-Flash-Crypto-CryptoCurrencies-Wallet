7d205f59eff5b0513c6bf93d0564e08af17cbd0c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:04 +0300

    SerialisationDebug.cs

---
+++ Electrum-Flash/SerialisationDebug.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "testObject", menuName = "Debug/serialization")]
+public class SerialisationDebug : ScriptableObject
+{
+    public OpinionDictionary opinions;
+}
