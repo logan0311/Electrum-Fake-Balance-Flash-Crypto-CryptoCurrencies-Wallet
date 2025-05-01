fa96c5476b15cd24a814f2da3e8d77d8ce03122a
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:21 +0300

    PerhapsNotFoundException.cs

---
+++ Electrum-Flash/Exceptions/PerhapsNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using Vulpes.Electrum.Domain.Monads;
+
+namespace Vulpes.Electrum.Domain.Exceptions;
+public class PerhapsNotFoundException : Exception
+{
+    public Type RequestedType { get; init; }
+
+    public PerhapsNotFoundException(Type requestedType)
+        : base($"The {nameof(Perhaps<Type>)} of type {requestedType.Name} is empty.")
+    {
+        RequestedType = requestedType;
+    }
+    public PerhapsNotFoundException(string message, Type requestedType)
+        : base(message)
+    {
+        RequestedType = requestedType;
+    }
+}
