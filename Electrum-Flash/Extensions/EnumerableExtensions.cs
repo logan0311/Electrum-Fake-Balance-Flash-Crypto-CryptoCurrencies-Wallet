6925d0730af5dbe6ef6da6247ca37eae7b13f1e7
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:31 +0300

    EnumerableExtensions.cs

---
+++ Electrum-Flash/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Vulpes.Electrum.Domain.Extensions;
+public static class EnumerableExtensions
+{
+    public static TObject GetRandom<TObject>(this IEnumerable<TObject> enumerable)
+    {
+        var randomIndex = new Random().Next(0, enumerable.Count() - 1);
+        return enumerable.ElementAt(randomIndex);
+    }
+}
