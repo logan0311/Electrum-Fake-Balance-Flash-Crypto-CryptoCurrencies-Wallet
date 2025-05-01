0e8fcef9b8b153ffab91ac5fa6018d10d1a2604e
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:14 +0300

    SaveToJsonFileCommand.cs

---
+++ Electrum-Flash/ConsoleCommanding/Commands/SaveToJsonFileCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using Vulpes.Electrum.Domain.Commanding;
+using Vulpes.Electrum.Domain.Security;
+
+namespace Vulpes.Electrum.Domain.Commands;
+public record SaveToJsonFileCommand(object Object, string Directory, string FileName) : Command();
+public class SaveToJsonFileCommandHandler : CommandHandler<SaveToJsonFileCommand>
+{
+    protected override Task<AccessResult> InternalValidateAccessAsync(SaveToJsonFileCommand command) => Task.FromResult(AccessResult.Success());
+    protected override Task InternalExecuteAsync(SaveToJsonFileCommand command)
+    {
+        if (!Directory.Exists(command.Directory))
+        {
+            _ = Directory.CreateDirectory(command.Directory);
+        }
+
+        var serializedObject = JsonSerializer.Serialize(command.Object);
+        File.WriteAllText(command.FileName, serializedObject);
+
+        return Task.CompletedTask;
+    }
+}
