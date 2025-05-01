d2fbc4bb44ec683b76524090ba78b596789f677b
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:20 +0300

    MediationHandlerNotFoundException.cs

---
+++ Electrum-Flash/Exceptions/MediationHandlerNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace Vulpes.Electrum.Domain.Exceptions;
+public class MediationHandlerNotFoundException : Exception
+{
+    public MediationHandlerNotFoundException(Type type) : base($"Mediation handler of type {type} was not found. It may be unregistered.")
+    { }
+}
