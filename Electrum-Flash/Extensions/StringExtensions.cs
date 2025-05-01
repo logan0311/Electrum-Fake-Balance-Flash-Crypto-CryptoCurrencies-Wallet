e6c896c47d54796c97968d4fdbd10ce2252a4422
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:35 +0300

    StringExtensions.cs

---
+++ Electrum-Flash/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Vulpes.Electrum.Domain.Extensions;
+public static class StringExtensions
+{
+    public static string ToTitleCase(this string value) => value.ToTitleCase("en-US");
+    public static string ToTitleCase(this string value, string cultureInfoName)
+    {
+        var textInfo = new CultureInfo(cultureInfoName, false).TextInfo;
+        return textInfo.ToTitleCase(value);
+    }
+}
