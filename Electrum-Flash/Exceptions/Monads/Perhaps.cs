fc017457d99965c51b47d765c43609a7d4647789
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:23 +0300

    Perhaps.cs

---
+++ Electrum-Flash/Exceptions/Monads/Perhaps.cs
@@ -0,0 +1,39 @@
+﻿using Vulpes.Electrum.Domain.Exceptions;
+
+namespace Vulpes.Electrum.Domain.Monads;
+public class Perhaps<TItem>
+{
+    public static Perhaps<TItem> Empty => new(default!, true);
+    public static Perhaps<TItem> ToPerhaps(TItem item)
+    {
+        // If the type is a string and it is an empty string, return an empty perhaps.
+        if (typeof(TItem) == typeof(string) && string.IsNullOrWhiteSpace(item as string))
+        {
+            return Empty;
+        }
+
+        return new Perhaps<TItem>(item);
+    }
+
+    private readonly TItem item;
+
+    public bool IsEmpty { get; init; } = true;
+
+    private Perhaps(TItem item, bool isEmpty)
+    {
+        this.item = item;
+        IsEmpty = isEmpty;
+    }
+
+    public Perhaps(TItem item)
+    {
+        this.item = item;
+        IsEmpty = item != null;
+    }
+
+    public TItem Get() => item;
+    public TItem Else(TItem contingency) => item ?? contingency;
+    public TItem ElseThrow(Exception exception) => item ?? throw exception;
+    public TItem ElseThrow(string message) => ElseThrow(new PerhapsNotFoundException(message, typeof(TItem)));
+    public TItem ElseThrow() => ElseThrow(new PerhapsNotFoundException(typeof(TItem)));
+}
