988066154f2640b8a4cc792dcd06a19d6a23eafb
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:51:52 +0300

    ElectrumDebugBehavior.cs

---
+++ Electrum-Flash/ElectrumDebugBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ElectrumDebugBehavior : MonoBehaviour
+{
+    public Character testCharacter;
+    public void MakeTestCharacterChooseAction(Character character)
+    {
+        character.ChooseAction();
+    }
+}
+
+[CustomEditor(typeof(ElectrumDebugBehavior))]
+public class ElectrumDebugEditor : Editor
+{
+    SerializedProperty testCharacter;
+
+    private void OnEnable()
+    {
+        testCharacter = serializedObject.FindProperty("testCharacter");
+    }
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        if (!Application.isPlaying) return;
+        ElectrumDebugBehavior script = (ElectrumDebugBehavior)target;
+        if (GUILayout.Button("Debug choose action"))
+        {
+            script.MakeTestCharacterChooseAction(script.testCharacter);
+        }
+    }
+}
