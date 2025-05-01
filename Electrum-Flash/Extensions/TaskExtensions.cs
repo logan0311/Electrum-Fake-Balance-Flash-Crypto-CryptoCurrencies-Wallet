cb6ff1f7aba0168b4a39a81b803bcc42f8ae535a
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:37 +0300

    TaskExtensions.cs

---
+++ Electrum-Flash/Extensions/TaskExtensions.cs
@@ -0,0 +1,5 @@
+﻿namespace Vulpes.Electrum.Domain.Extensions;
+public static class TaskExtensions
+{
+    public static Task<TResult> FromResult<TResult>(this TResult item) => Task.FromResult(item);
+}
