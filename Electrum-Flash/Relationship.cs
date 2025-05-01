51ec4622257906a8a3abe6f0fa1b5d6d912188d6
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:03 +0300

    Relationship.cs

---
+++ Electrum-Flash/Relationship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//relationship represent a character "mode of interaction" towards another, they don't strictly tie to the characters opinions of each other (a character may act friendly to someone they don't like for various reason) they don't value either
+//Later on having relationship that are more intensive versions of others might be good, but for now it's easily expressed with conditions
+public enum RelationshipType
+{
+  self,
+  aquaintance,
+  friend,
+  goodFriend,
+  ally,
+  lover,
+  rival,
+  enemy,
+  parent,
+  child,
+  sibling,
+  liege,
+  vassal
+}
+ 
