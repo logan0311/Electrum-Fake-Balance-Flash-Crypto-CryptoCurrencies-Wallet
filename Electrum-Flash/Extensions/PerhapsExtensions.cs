f12f31edf2946e5fb9d33f2bf2186dfc582a6bfb
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:33 +0300

    PerhapsExtensions.cs

---
+++ Electrum-Flash/Extensions/PerhapsExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Vulpes.Electrum.Domain.Monads;
+
+namespace Vulpes.Electrum.Domain.Extensions;
+public static class PerhapsExtensions
+{
+    public static Perhaps<TItem> FirstOrPerhaps<TItem>(this IEnumerable<TItem> items)
+    {
+        using (var enumerator = items.GetEnumerator())
+        {
+            if (enumerator.MoveNext())
+            {
+                return Perhaps<TItem>.ToPerhaps(enumerator.Current);
+            }
+        }
+
+        return Perhaps<TItem>.Empty;
+    }
+
+    public static Perhaps<TItem> FirstOrPerhaps<TItem>(this IEnumerable<TItem> items, Func<TItem, bool> predicate)
+    {
+        using (var enumerator = items.Where(predicate).GetEnumerator())
+        {
+            if (enumerator.MoveNext())
+            {
+                return Perhaps<TItem>.ToPerhaps(enumerator.Current);
+            }
+        }
+
+        return Perhaps<TItem>.Empty;
+    }
+
+    public static Perhaps<TItem> LastOrPerhaps<TItem>(this IEnumerable<TItem> items)
+    {
+        var choice = Perhaps<TItem>.Empty;
+
+        foreach (var item in items)
+        {
+            choice = Perhaps<TItem>.ToPerhaps(item);
+        }
+
+        return choice;
+    }
+
+    public static Perhaps<TItem> LastOrPerhaps<TItem>(this IEnumerable<TItem> items, Func<TItem, bool> predicate)
+    {
+        var choice = Perhaps<TItem>.Empty;
+
+        foreach (var item in items.Where(predicate))
+        {
+            choice = Perhaps<TItem>.ToPerhaps(item);
+        }
+
+        return choice;
+    }
+}
