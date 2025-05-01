f33dbf04ff5e0a189e4a96153bfa62ed0dad778a
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:30 +0300

    DictionaryExtensions.cs

---
+++ Electrum-Flash/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Vulpes.Electrum.Domain.Extensions;
+public static class DictionaryExtensions
+{
+    public static Dictionary<TKey, TValue> AddAndReturn<TKey, TValue>(this Dictionary<TKey, TValue> baseDictionary, KeyValuePair<TKey, TValue> pair)
+        where TKey : notnull
+    {
+        baseDictionary.Add(pair);
+        return baseDictionary;
+    }
+
+    public static Dictionary<TKey, TValue> AddAndReturn<TKey, TValue>(this Dictionary<TKey, TValue> baseDictionary, TKey key, TValue value)
+        where TKey : notnull
+        => baseDictionary.AddAndReturn(new(key, value));
+
+    public static void Add<TKey, TValue>(this Dictionary<TKey, TValue> baseDictionary, KeyValuePair<TKey, TValue> pair)
+        where TKey : notnull
+        => baseDictionary.Add(pair.Key, pair.Value);
+
+    public static void Add<TKey, TValue>(this Dictionary<TKey, TValue> baseDictionary, params (TKey, TValue)[] pairs)
+        where TKey : notnull
+    {
+        foreach (var pair in pairs)
+        {
+            baseDictionary.Add(pair.Item1, pair.Item2);
+        }
+    }
+}
