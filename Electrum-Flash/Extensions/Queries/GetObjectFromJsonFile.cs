8ab1afaa27914361855af7f80ad123aeca9f9238
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:40 +0300

    GetObjectFromJsonFile.cs

---
+++ Electrum-Flash/Extensions/Queries/GetObjectFromJsonFile.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Vulpes.Electrum.Domain.Monads;
+using Vulpes.Electrum.Domain.Querying;
+
+namespace Vulpes.Electrum.Domain.Queries;
+public record GetObjectFromJsonFile(Type SerializeType, string ObjectName) : Query;
+public class GetObjectFromJsonFileHandler : QueryHandler<GetObjectFromJsonFile, object>
+{
+    protected async override Task<object> InternalRequestAsync(GetObjectFromJsonFile query)
+    {
+        using var streamReader = new StreamReader(query.ObjectName);
+        var text = Perhaps<string>.ToPerhaps(await streamReader.ReadToEndAsync()).ElseThrow($"Failed to retrieve data from {query.ObjectName}.");
+        var result = JsonSerializer.Deserialize(text, query.SerializeType)!;
+
+        return result;
+    }
+}
