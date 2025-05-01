21d9c33f911922385fa606c0e90eca6c55a538cf
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:26 +0300

    QueryHandler.cs

---
+++ Electrum-Flash/Exceptions/Querying/QueryHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Vulpes.Electrum.Domain.Querying;
+public abstract class QueryHandler<TQuery, TResponse>
+    where TQuery : Query
+{
+    protected abstract Task<TResponse> InternalRequestAsync(TQuery query);
+
+    /// <summary>
+    /// Requests data from the provided query. Task is virtual to allow customization.
+    /// </summary>
+    public virtual async Task<TResponse> RequestAsync(TQuery query) => await InternalRequestAsync(query);
+}
