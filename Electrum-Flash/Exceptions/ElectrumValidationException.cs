237306f82eefe2485d89120e0c5127892298e38c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:18 +0300

    ElectrumValidationException.cs

---
+++ Electrum-Flash/Exceptions/ElectrumValidationException.cs
@@ -0,0 +1,8 @@
+﻿using Vulpes.Electrum.Domain.Security;
+
+namespace Vulpes.Electrum.Domain.Exceptions;
+public class ElectrumValidationException : Exception
+{
+    public ElectrumValidationException(ElectrumValidationResult electrumValidationResult) : base(string.Join(", ", electrumValidationResult.Messages))
+    { }
+}
