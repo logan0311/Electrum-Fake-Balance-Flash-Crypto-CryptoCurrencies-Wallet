63d95d6f4c9092cefdfed493b1fea9aa9bfb506c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:16 +0300

    AccessDeniedException.cs

---
+++ Electrum-Flash/Exceptions/AccessDeniedException.cs
@@ -0,0 +1,12 @@
+﻿using Vulpes.Electrum.Domain.Security;
+
+namespace Vulpes.Electrum.Domain.Exceptions;
+public class AccessDeniedException : Exception
+{
+    public AccessResult AccessResult { get; init; } = AccessResult.Empty;
+
+    public AccessDeniedException(AccessResult accessResult) : base(accessResult.Message)
+    {
+        AccessResult = accessResult;
+    }
+}
