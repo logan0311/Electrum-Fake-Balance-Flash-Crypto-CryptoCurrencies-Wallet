6d3e4a461bb8f666f553478833ca8f00a6a789a7
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:53:45 +0300

    IMediator.cs

---
+++ Electrum-Flash/Mediation/IMediator.cs
@@ -0,0 +1,14 @@
+﻿using Vulpes.Electrum.Domain.Commanding;
+using Vulpes.Electrum.Domain.Querying;
+
+namespace Vulpes.Electrum.Domain.Mediation;
+public interface IMediator
+{
+    Task<TResponse> RequestResponseAsync<TQuery, TResponse>(TQuery query)
+        where TQuery : Query;
+
+    Task ExecuteCommandAsync<TCommand>(TCommand command)
+        where TCommand : Command;
+    Task<bool> EvaluateAccessAsync<TCommand>(TCommand command)
+        where TCommand : Command;
+}
