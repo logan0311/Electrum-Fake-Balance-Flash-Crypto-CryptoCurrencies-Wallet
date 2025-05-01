5dafd50c7919d6595d45e416dd16e6b1fe9d755e
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:51:44 +0300

    ActionSet.cs

---
+++ Electrum-Flash/ActionSet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="new Action set", menuName = "Electrum/Action Set")]
+public class ActionSet : ScriptableObject
+{
+    public List<Action> actions = new List<Action>();
+
+}
